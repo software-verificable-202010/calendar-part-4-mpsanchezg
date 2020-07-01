897a9518346f29f7a28088ab64be634f9d778d83
mpsanchezg <mapisanchezg@gmail.com>
2020-07-01 13:11:29 -0400

    first commit

---
+++ Tests/Model/CalendarModelTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CalendarApp.Model;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class CalendarModelTests
+    {
+        private CalendarModel calendarModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            calendarModel = new CalendarModel(2020, 12);
+        }
+
+        [Test]
+        public void TestShouldInstanciateCalendarMonthModelWithGivenMonthNumber()
+        {
+            Assert.IsTrue(calendarModel.CalendarMonth.MonthNumber == 12);
+        }
+
+        [Test]
+        public void TestShouldInstanciateCalendarMonthModelWithGivenYearNumber()
+        {
+            Assert.IsTrue(calendarModel.CalendarMonth.YearOfMonth == 2020);
+        }
+    }
+}
